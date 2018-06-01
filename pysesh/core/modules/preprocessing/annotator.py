e76287841785cbb71355eb07e93080dfc3c8ad2a
Kaan Eraslan <kaaneraslan@gmail.com>
2018-06-01 13:51:50 +0200

    Add selectors
    Polygon selector and rectangular selectors are added. Need to test if both
    work.

--- pysesh/core/modules/preprocessing/annotator.py
+++ pysesh/core/modules/preprocessing/annotator.py
@@ -19,7 +19,7 @@ class Annotator_init(QtDesignerImageWindow):
         super().setupUi(self.main_window)
         #
 
-class ViewerNew(QtWidgets.QGraphicsView):
+class RectViewer(QtWidgets.QGraphicsView):
     """
     Overwrite methods of graphics view for selectors
 
@@ -51,7 +51,52 @@ class ViewerNew(QtWidgets.QGraphicsView):
         self.changeRubberBand = False
         QtWidgets.QGraphicsView.mouseReleaseEvent(self, event)
     #
-# Final Interface -----------------------------
+
+class PolygonViewer(QtWidgets.QGraphicsView):
+    """
+    Overwrite methods of graphics view for handingling polygon selector
+    """
+    polyChanged = QtCore.pyqtSignal([])
+    def __init__(self, *args, **kwards):
+        QtWidgets.QGraphicsView.__init__(self, *args, **kwards)
+        self.scene = QtWidgets.QGraphicsScene()
+        self.setScene(self.scene)
+        self.setMouseTracking(True)
+        self.setSceneRect(QtCore.QRectF(self.viewport().rect()))
+        #
+        self.points = []
+    #
+    def mousePressEvent(self, event):
+        "Override the mouse press event for collecting points"
+        event_position = event.pos()
+        point = QtCore.QPointF(self.mapToScene(event_position))
+        self.points.append(point)
+        QtWidgets.QGraphicsView.mousePressEvent(self, event)
+        #
+    #
+    def getPolygon(self):
+        "Get polygon from the list of points"
+        polygon = QtGui.QPolygonF()
+        for point in self.points:
+            polygon.append(point)
+        #
+        if polygon.isClosed() != True:
+            polygon.append(self.points[0])
+            self.points.append(self.points[0])
+            self.polyChanged.emit(self.points)
+        #
+        return polygon
+    #
+    def mouseReleaseEvent(self, event):
+        "Draw the polygon at mouse realse"
+        #
+        polygon = self.getPolygon()
+        self.scene.addPolygon(polygon)
+        QtWidgets.QGraphicsView.mouseReleaseEvent(self, event)
+        #
+        return None
+    #
+# Final Interface ----------------------------
 
 class Annotator_final(Annotator_init):
     "Final version of the interface"
@@ -75,12 +120,19 @@ class Annotator_final(Annotator_init):
         # ---- Image Operations -----------
         #
         self.image_dict = {}
-        self.viewer_new = ViewerNew()
-        self.GraphicWidget.addWidget(self.viewer_new)
-        self.viewer_new.rectChanged.connect(self.onRectChanged)
+        self.rect_viewer = RectViewer()
+        self.rect_viewer.rectChanged.connect(self.getRectCoordinates)
+        self.poly_viewer = PolygonViewer()
+        self.poly_viewer.polyChanged.connect(self.getPolyCoordinates)
+        self.default_viewer = QtWidgets.GraphicView()
+        self.GraphicWidget.insertWidget(0, self.default_viewer)
         self.graphicsScene = QtWidgets.QGraphicsScene()
         self.pixmap_image = QtGui.QPixmap()
         #
+        # Radio Buttons
+        #
+        self.boxSelectorRButton.toggled.connect(self.setRectView)
+        self.polygonSelectorRButton.toggled.connect(self.setPolygonView)
     #
     # #### Standard Gui Elements ####
     #
@@ -160,6 +212,23 @@ class Annotator_final(Annotator_init):
     #
     # GraphicView Widget
     #
+    def setRectView(self):
+        "Set rectangulare viewer"
+        if self.boxSelectorRButton.isChecked():
+            # remove viewer item at 0
+            self.GraphicWidget.takeAt(0)
+            # add rect viewer
+            self.GraphicWidget.insertWidget(0, self.rect_viewer)
+    #
+    def setPolygonView(self):
+        "Sets the polygon viewer"
+        if self.polygonSelectorRButton.isChecked():
+            # remove viewer item at 0
+            self.GraphicWidget.takeAt(0)
+            # add rect viewer
+            self.GraphicWidget.insertWidget(0, self.poly_viewer)
+        #
+    #
     def get_image(self):
         """
         gets the selected image from the widget list
@@ -199,21 +268,29 @@ class Annotator_final(Annotator_init):
                                self.pixmap_image.height())
         self.graphicsScene.setSceneRect(0,0, width_px, height_px)
         self.graphicsScene.addPixmap(self.pixmap_image)
-        self.viewer_new.setSceneRect(0, # item position x
-                                       0, # item position y
-                                       width_px, height_px)
-        self.viewer_new.setScene(self.graphicsScene)
-        self.viewer_new.show()
+        viewer = self.GraphicWidget.itemAt(0)
+        viewer.setSceneRect(0, # item position x
+                            0, # item position y
+                            width_px, height_px)
+        viewer.setScene(self.graphicsScene)
+        viewer.show()
         #
         return None
     #
-    def onRectChanged(self, r):
+    def getRectCoordinates(self, r):
         topLeft = r.topLeft()
         bottomRight = r.bottomRight()
         print(topLeft.x(), topLeft.y(), bottomRight.x(), bottomRight.y())
         # TODO change this to be active if the radio button is selected
         # and take the coordinates and added to the dictionary of the
         # image in the image dict
+    #
+    def getPolyCoordinates(self, point_list: []):
+        "Recieve polygon coordinates"
+        for index, point in enumerate(point_list):
+            print("point ", index)
+            print("point coordinate x: ", point.x)
+            print("point coordinate y: ", point.y)
 
 
 
