167fe7d43d0ea066833d0a4facd994883c42cd01
Kaan Eraslan <kaaneraslan@gmail.com>
2018-12-03 01:30:53 +0100

    Added a line segmenting example with hieratic.
    The core code needs to be tested. I should arrange the tests as well
    
    Demotic and hieroglyphic examples would be good as well.

---
+++ test/ImageOperationsTests/test_segmenters.py
@@ -0,0 +1,12 @@
+import unittest
+
+current_path = os.getcwd()
+tests_path = current_path + os.sep + os.pardir
+pysesh_main_path = tests_path + os.sep + os.pardir
+pysesh_path = pysesh_main_path + os.sep + "pysesh"
+core_path = pysesh_path + os.sep + "core"
+modules_path = core_path + os.sep + "modules"
+preprocess_path = modules_path + os.sep + "preprocessing"
+interface_path = preprocess_path + os.sep + "interface.html"
+
+
